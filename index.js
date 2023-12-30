c6be97f83ff748d614e58609d36c79f74efbaa9f
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2023-12-30 09:13:35 +0100

    deleted some line of code

--- index.js
+++ index.js
@@ -3,18 +3,15 @@
 function openNav() {
   const sidebar = document.getElementById("sidebar");
   const content = document.getElementById("content");
-  const userInfo = document.getElementById("user-info");
 
   if (sidebar.style.width === "250px") {
     // Close the sidebar if it's already open
     sidebar.style.width = "0";
     content.style.marginLeft = "0";
-    userInfo.style.display = "none"; // Hide user-info
   } else {
     // Open the sidebar
     sidebar.style.width = "250px";
     content.style.marginLeft = "250px";
-    userInfo.style.display = "block"; // Show user-info
   }
 
   document.body.addEventListener("click", closeNavOutside);
@@ -24,7 +21,6 @@ function closeNavOutside(event) {
   // Close the sidebar if the click is outside the sidebar and not on the "open" button
   const sidebar = document.getElementById("sidebar");
   const content = document.getElementById("content");
-  const userInfo = document.getElementById("user-info");
 
   if (
     !event.target.closest("#sidebar") &&
@@ -32,7 +28,6 @@ function closeNavOutside(event) {
   ) {
     sidebar.style.width = "0";
     content.style.marginLeft = "0";
-    userInfo.style.display = "none"; // Hide user-info
     document.body.removeEventListener("click", closeNavOutside);
   }
 }
